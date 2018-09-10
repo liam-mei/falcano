79a1369996c85bed623808062fced7723c577009
Sam-Park <sam.park.email@gmail.com>
2018-09-10 14:15:49 -0400

    init commit, created multiple components, bare bones

---
+++ flight-logs/src/components/AircraftViewModal.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Modal from 'react-modal';
+class AircraftViewModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      data: "",
+     }
+  }
+  render() { 
+    return ( 
+      <div>
+        {/* title */}
+        {/* IMG of aircraft*/}
+        {/* data */}
+      </div>
+     );
+  }
+}
+ 
+export default AircraftViewModal;
---
+++ flight-logs/src/components/Flights.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Flights extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return ( 
+      <div>
+        <NavBar /> 
+        
+      </div>
+     );
+  }
+}
+ 
+export default Flights;
