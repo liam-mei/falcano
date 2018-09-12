73ebb12bd788775f932954b58d4ca9bbbef8969e
derrick-mei <derrick.mei.168@gmail.com>
2018-09-12 17:42:55 -0500

    changed verbose plural name on flights model

--- backend/flights/models.py
+++ backend/flights/models.py
@@ -10,6 +10,9 @@ class Flights(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name_plural = "flights"
+
     def __str__(self):
         return self.name
 
