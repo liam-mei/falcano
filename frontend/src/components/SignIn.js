d0d5766bd33542c19c5414fbc982067cb22a15df
derrick-mei <derrick.mei.168@gmail.com>
2018-09-14 11:47:06 -0500

    added redirect to signup

--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -2,86 +2,89 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
 class SignIn extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			username: '',
-			password: '',
-			errorMessage: '',
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      errorMessage: ''
+    };
+  }
 
-	handleChange = (e) => {
-		this.setState({ [e.target.name]: e.target.value, errorMessage: '' });
-	};
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value, errorMessage: '' });
+  };
 
-	handleSubmit = (e) => {
-		e.preventDefault();
-		//check the username are not empty
-		if (!this.state.username) {
-			this.setState({ errorMessage: 'Please enter a username' });
-			return;
-		}
+  handleSubmit = (e) => {
+    e.preventDefault();
+    //check the username are not empty
+    if (!this.state.username) {
+      this.setState({ errorMessage: 'Please enter a username' });
+      return;
+    }
 
-		//check the password field is not empty
-		if (!this.state.password) {
-			this.setState({ errorMessage: 'Please enter a password' });
-			return;
-		}
-		// ready to sign in
-		const user = {
-			username: this.state.username,
-			password: this.state.password,
-		};
-		axios
-			.post('https://flightloggercs10.herokuapp.com/token-auth/', user)
-			.then((response) => {
-				// set the token to local storage
-				localStorage.setItem('token', response.data.token);
-				this.setState({
-					username: '',
-					password: '',
-					errorMessage: '',
-				});
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	};
+    //check the password field is not empty
+    if (!this.state.password) {
+      this.setState({ errorMessage: 'Please enter a password' });
+      return;
+    }
+    // ready to sign in
+    const user = {
+      username: this.state.username,
+      password: this.state.password
+    };
+    axios
+      .post('https://flightloggercs10.herokuapp.com/token-auth/', user)
+      .then((response) => {
+        // set the token to local storage
+        localStorage.setItem('token', response.data.token);
+        this.setState({
+          username: '',
+          password: '',
+          errorMessage: ''
+        });
+        this.props.history.push('/');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-	render() {
-		return (
-			<div>
-				<form onSubmit={this.handleSubmit}>
-					<input
-						onChange={this.handleChange}
-						value={this.state.username}
-						name="username"
-						type="text"
-						className="form-control"
-						placeholder="Username"
-					/>
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            onChange={this.handleChange}
+            value={this.state.username}
+            name="username"
+            type="text"
+            className="form-control"
+            placeholder="Username"
+          />
 
-					<input
-						onChange={this.handleChange}
-						value={this.state.password}
-						name="password"
-						type="password"
-						className="form-control"
-						placeholder="password"
-					/>
+          <input
+            onChange={this.handleChange}
+            value={this.state.password}
+            name="password"
+            type="password"
+            className="form-control"
+            placeholder="password"
+          />
 
-					<button>Sign In</button>
-					<div style={{ color: 'red' }}>{this.state.errorMessage ? this.state.errorMessage : ''}</div>
-				</form>
-			</div>
-		);
-	}
+          <button>Sign In</button>
+          <div style={{ color: 'red' }}>
+            {this.state.errorMessage ? this.state.errorMessage : ''}
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 SignIn.propTypes = {
-	username: PropTypes.string,
-	password: PropTypes.string,
+  username: PropTypes.string,
+  password: PropTypes.string
 };
 
 export default SignIn;
