b2e76810585458a4b52a92de697ce9d3f370ef00
Bee C <b.choeung@gmail.com>
2018-09-13 20:35:50 -0400

    worked on Signin & Signup frontend

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,25 +1,93 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 class SignUp extends Component {
   constructor(props) {
     super(props);
-    this.state = {  
-      email: "",
+    this.state = {
+      username: "",
       password: "",
-    }
+      confirmpassword: "",
+      errorMessage:"",
+    };
   }
-  render() { 
-    return ( 
-      <div> 
-      <form>
-        <input placeholder="email">
-        </input>
-        <input placeholder="password">
-        </input>
-      </form>
+
+  handleChange = e => {
+
+    this.setState({ [e.target.name]: e.target.value, errorMessage: "" });
+  };
+
+  handleSubmit = e =>
+  {
+    
+    e.preventDefault();
+    //check the username are not empty
+    if ( this.state.username )
+    {
+      //check the password field is not empty
+      if ( !this.state.password )
+      {
+        this.setState({ errorMessage: "Please enter a password" });
+      }
+      //check if the passwords match
+      if ( this.state.password !== this.state.confirmpassword ) {
+        this.setState({ errorMessage: "The password does not match" });
+      } else {
+        // ready to sign up
+        const user={
+          username: this.state.username,
+          password: this.state.password
+        }
+        axios.post("https://flightloggercs10.herokuapp.com/signup/", user).then(response =>{
+          console.log(response.data);
+        }).catch(err => {
+          console.log(err);
+        })
+      }
+    } else
+    {
+      this.setState({ errorMessage: "Please enter a username" });
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            onChange={this.handleChange}
+            value={this.state.username}
+            name="username"
+            type="text"
+            className="form-control"
+            placeholder="Username"
+          />
+
+          <input
+            onChange={this.handleChange}
+            value={this.state.password}
+            name="password"
+            type="password"
+            className="form-control"
+            placeholder="password"
+          />
+
+          <input
+            onChange={this.handleChange}
+            value={this.state.confirmpassword}
+            name="confirmpassword"
+            type="password"
+            className="form-control"
+            placeholder="confirm password"
+          />
+
+          <button>Sign Up</button>
+          <div style={{color: "red"}}>
+            {(this.state.errorMessage) ? this.state.errorMessage:""}
+          </div>
+        </form>
       </div>
-     );
+    );
   }
 }
- 
+
 export default SignUp;
