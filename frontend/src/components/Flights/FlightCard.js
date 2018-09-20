7152e0421d02a8f65d1d7f4f530708abb79c3e1f
Bee C <b.choeung@gmail.com>
2018-09-20 18:55:39 -0400

    created FlightCard with placeholder info

---
+++ frontend/src/components/Flights/FlightCard.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './FlightCard.css';
+
+class FlightCard extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+	render() {
+		return (
+        <div className="FlightCard-Wrapper">
+            <h3>Crosscountry Solo</h3>
+            <p>KGAI-KLNS-KGAI</p>
+            <p>N911AT</p>
+            <img src="//unsplash.it/250px/150px" className="FlightCard-Image"/>
+            <div className="FLightCard-Hours-Date">
+                <span>3-17-18</span>
+                <span>1.6 hrs</span>
+            </div>
+        </div>
+		);
+	}
+}
+
+export default FlightCard;
