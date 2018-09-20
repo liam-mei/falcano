65ff143f8cd4883afa1ab4a8b1e14ab16fb00d36
derrick-mei <derrick.mei.168@gmail.com>
2018-09-20 05:52:01 -0500

    added functionality to billing page to make token

--- frontend/src/components/Billing/BillingForm.js
+++ frontend/src/components/Billing/BillingForm.js
@@ -6,7 +6,7 @@ import { Elements, StripeProvider } from 'react-stripe-elements';
 
 const BillingForm = () => {
   return (
-    <StripeProvider apiKey="pk_test_TYooMQauvdEDq54NiTphI7jx">
+    <StripeProvider apiKey="pk_test_tYvByOjo3u4ZLzcoJYTe4NHT">
       <div>
         <Elements>
           <CheckoutForm />
@@ -17,3 +17,52 @@ const BillingForm = () => {
 };
 
 export default BillingForm;
+
+
+// import React, { Component } from 'react';
+
+// import { StripeProvider } from 'react-stripe-elements';
+
+
+// import { Elements } from 'react-stripe-elements';
+// import CheckoutForm from './CheckoutForm';
+
+// //StripeProvider gives us access to the Stripe Object
+// //i.e Stripe.createToken, stripe.elements() etc
+// //App loads the stripe script asynchronously in CDM
+
+// class BillingForm extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { stripe: '' };
+//   }
+//   componentDidMount() {
+//     if (window.Stripe) {
+//       this.setState({
+//         stripe: window.Stripe(process.env.REACT_APP_publishable)
+//       });
+//     } else {
+//       document.querySelector('#stripe-js').addEventListener('load', () => {
+//         //Create Stripe instance once Stripe.js loads
+//         this.setState({
+//           stripe: window.Stripe(process.env.REACT_APP_publishable)
+//         });
+//       });
+//     }
+//   }
+
+//   render() {
+//     return (
+//       this.state.stripe && (
+//         <StripeProvider stripe={this.state.stripe}>
+//           {/* <BillingForm /> */}
+//           <Elements>
+//             <CheckoutForm />
+//           </Elements>
+//         </StripeProvider>
+//       )
+//     );
+//   }
+// }
+
+// export default BillingForm;
