8f1d01804a4ec11ee21aec44e3da3fa09171cbd6
Sam-Park <sam.park.email@gmail.com>
2018-09-25 13:53:37 -0400

    jwt authentication

--- frontend/src/components/Authenication/Auth.js
+++ frontend/src/components/Authenication/Auth.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import { isLoggedIn } from '../../utils/helper/helperFuncions';
+const dev = true;
+	let URL
+	(dev ? URL = "http://127.0.0.1:8000/api-token-verify/"
+		: URL = "https://flightloggercs10.herokuapp.com/api-token-verify/");
+
 
 const Auth = (Comp) =>
 	class extends Component {
@@ -9,16 +14,24 @@ const Auth = (Comp) =>
 		};
 
 		componentDidMount() {
-			if (isLoggedIn()) {
-				this.setState({ loggedIn: true });
-			} else {
-				this.props.history.push('/');
-				window.location.reload();
+			let token = localStorage.getItem('token')
+
+			axios({
+				method: 'POST',
+				url: URL,
+				data: {
+					"token": token
+				}
+				}).then((response) => {
+				console.log("auth response ", response)
+					this.setState({ loggedIn: true })
+				}).catch(err => {
+					console.log(err)
+				})
 			}
-		}
-		render() {
-			return <Comp loggedIn={this.state.loggedIn} />;
-		}
-	};
+			render() {
+						return <Comp loggedIn={this.state.loggedIn} />;
+					}
+				};
 
 export default Auth;
