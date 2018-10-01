fee80147538728dce52e487b697fa8f9203fdf07
derrick-mei <derrick.mei.168@gmail.com>
2018-10-01 12:08:07 -0500

    added error handling when already signed in to display different button

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -9,10 +9,27 @@ import './LandingPage.css';
 import '../utils/Images/landingPage.svg';
 import logo from '../utils/Images/Logo.svg';
 
+import Auth from "./Authenication/Auth"
+
 class LandingPage extends Component {
 	render() {
 		return (
-			<div className="LandingPage">
+			<div>
+			{this.props.loggedIn ? <div className="LandingPage">
+			<div className="LandingPage-card">
+				<img src={logo } className="Falcano-Logo" alt="logo"/>
+				<h3 className="card-slogan">Time flies</h3>
+				<p className="card-mission-statement">
+				Gone are the days of untracked flights.  Here at <strong>Falcano</strong> we make it easy for you to track your flights.  Simply click and trips, instructors, hours and much more are easily managed.  We've even made it easy for you to visually track your trips with a SkyVector map.  Take us on your next flight and see what all the fuss is about.
+				</p>
+				<div className="card-registration">
+					<Link className="registration-signin" to={'/home'}>
+						Home
+					</Link>
+				</div>
+			</div>
+		</div> : 
+				<div className="LandingPage">
 				<div className="LandingPage-card">
 					<img src={logo } className="Falcano-Logo" alt="logo"/>
 					<h3 className="card-slogan">Time flies</h3>
@@ -28,9 +45,11 @@ class LandingPage extends Component {
 						</Link>
 					</div>
 				</div>
+			</div>}
 			</div>
+			
 		);
 	}
 }
 
-export default LandingPage;
+export default Auth(LandingPage);
