c5abd6bb057c51662f95840bb3e51451c69c97e7
Sam-Park <sam.park.email@gmail.com>
2018-10-02 21:31:38 -0400

    I think I fixed min max  issues

--- frontend/src/components/User/Instructors.js
+++ frontend/src/components/User/Instructors.js
@@ -148,67 +148,15 @@ class Instructors extends React.Component {
       <div className="Instructors">
         <TopHeader breadcrumb={["settings"]} />
         <NavBar />
-        {/* <div className="Instructors-container">
-				<div className="Instructor-card ">
-					<div className="card-name">Charles Martinez</div>
-					<div className="card-number">141423523537</div>
-					<div className="card-img">
-						<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFVt2jtKbzaWOVWCNUPGs_Uwy0UccqHala9MU19ygYBeSmpeuk" />
-					</div>
-					<div className="card-description">
-						<div className="description-title">Descriptions/Notes</div>
-						<div className="description-content">
-							Charles is one of newest memeber of Falcano Flight Services
-              Instuctors Staff. He holds the following.
-            </div>
-					</div>
-					<div className="card-ratings">
-						<div className="ratings-title">Ratings:</div>
-						<div className="ratings-details">CFI,CFII,MEI</div>
-					</div>
-					<div className="card-contact">
-						<div className="contact-title">Contact</div>
-						<div className="contact-info">
-							<div className="contact-email">charlesM@yahoo.com</div>
-							<div className="contact-number">301-123-4567</div>
-						</div>
-					</div>
-				</div>
-				<div className="Instructor-card ">
-					<div className="card-name">Mary Finn</div>
-					<div className="card-number">1414235235323</div>
-					<div className="card-img">
-						<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbTteGkDxbUM_3CuLDlWVrBAMlKxIqt0FGg86BR6fMLP4zoaW2pQ" />
-					</div>
-					<div className="card-description">
-						<div className="description-title">Descriptions/Notes</div>
-						<div className="description-content">
-							Mary is one of the memebers of Falcano Flight Services Instuctors
-              Staff. He holds the following.
-            </div>
-					</div>
-					<div className="card-ratings">
-						<div className="ratings-title">Ratings:</div>
-						<div className="ratings-details">CFI,CFII,MEI</div>
-					</div>
-					<div className="card-contact">
-						<div className="contact-title">Contact</div>
-						<div className="contact-info">
-							<div className="contact-email">MaryF@yahoo.com</div>
-							<div className="contact-number">501-123-4567</div>
-						</div>
-					</div>
-				</div> */}
         <div className="InstructorCard">
           {this.state.instructors.map(instr => {
             return (
               <InstructorCard  key={instr.id} data={instr}>
                 name: {instr.name}
-                asas
               </InstructorCard>
             );
           })}
-          <Card style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '400px'}}>
+          <Card style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '400px', maxHeight: "568px"}}>
           <i class="fa fa-plus-circle Plus-sign" aria-hidden="true" onClick={this.toggle}></i>
           </Card> 
         </div>
